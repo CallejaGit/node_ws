4c0c7ec8f2db07ac8507880bf2a49da51ac54d56
Patrick <patrick.calleja@mail.utoronto.ca>
2017-11-10 13:37:15 -0500

    build a test imu node

---
+++ build/utra_nodes/CMakeFiles/imu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imu.dir/src/imu.cpp.o"
+  "/home/patrick/Documents/UTRA/node_ws/devel/lib/utra_nodes/imu.pdb"
+  "/home/patrick/Documents/UTRA/node_ws/devel/lib/utra_nodes/imu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/imu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/utra_nodes/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/patrick/Documents/UTRA/node_ws/src/utra_nodes
+# Build directory: /home/patrick/Documents/UTRA/node_ws/build/utra_nodes
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
